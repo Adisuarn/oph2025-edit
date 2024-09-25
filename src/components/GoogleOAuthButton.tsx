7923ba054ad447a485597ef8cb7ffc0b7b4854d3
Maki Kainan <makikainan04@gmail.com>
2024-09-25 20:51:50 +0700

    app page branch

--- src/components/GoogleOAuthButton.tsx
+++ src/components/GoogleOAuthButton.tsx
@@ -15,7 +15,7 @@ const GoogleOAuthButton = () => {
             }
         }
     } 
-    className='underline mt-5 bg-pink-300 py-4 px-6 rounded-full text-xl'>
+    className='underline mt-5 transition duration-0 ease-out bg-gradient-to-tr from-pink-300 to-pink-400 py-4 px-6 rounded-full text-xl shadow-lg hover:transition-all hover:from-pink-400 hover:to-pink-300 hover:duration-500'>
         Sign in with Google
     </button>
   )
