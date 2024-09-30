bab0c0fedfab13df5b1929e0c28e9783da12ac4c
Adisuarn <adisuarn.w@gmail.com>
2024-09-30 20:44:07 +0700

    Add Validation

---
+++ src/libs/validate.ts
@@ -0,0 +1,13 @@
+import { t } from 'elysia'
+
+export function UnionField(required: boolean, errorMsg: string, fields: string[]){
+  return required
+    ? t.Union(fields.map(field => t.Literal(field, { error: errorMsg })))
+    : t.Optional(t.Union(fields.map(field => t.Literal(field, { error: errorMsg }))))
+}
+
+export function StringField(required: boolean, errorMsg: string){
+  return required
+    ? t.String({ error: errorMsg })
+    : t.Optional(t.String({ error: errorMsg }))
+}
