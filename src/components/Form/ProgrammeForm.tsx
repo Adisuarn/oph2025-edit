3ff78eaf819dbe1052965606ef3da3cc722a250f
Sirut Krizruenont <siruj2201@gmail.com>
2024-10-13 13:26:04 +0700

    programe form test

--- src/components/Form/ProgrammeForm.tsx
+++ src/components/Form/ProgrammeForm.tsx
@@ -26,41 +26,101 @@ const onSubmit = (values: typeof initialValues, { setSubmitting, resetForm }: { 
 
 const ProgrammeForm: React.FC<{}> = () => {
   return (
-    <section>
-      <button>ย้อนกลับ</button>
-
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={onSubmit}
-      >
-        {({ isSubmitting }) => (
-        <Form className=' flex flex-col justify-center items-center space-y-4 p-5 bg-blue-100'>
-          <div>
-            <label htmlFor="textField1">การสมัครและการสอบเข้า:</label>
-            <Field type="text" name="textField1" className=' bg-green-200 rounded-xl' />
-            <ErrorMessage name="textField1" />
+    <main>
+      <div className='bg-sky-400'>
+        <button>ย้อนกลับ</button>
+          <div className=' flex justify-around'>
+            <p>สถานะ</p>
+              <div>
+                <button className='bg-grumpyGreen-300 mr-3 px-4 py-2 rounded-full'>preview</button>
+                <button className='bg-grumpyGreen-300 px-4 py-2 rounded-full'>ส่งการแก้ไข</button>
+              </div>
           </div>
+      </div>
 
-          <div>
-            <label htmlFor="textField2">วิชาหรือหลักสูตรที่เรียนเพิ่มเติม:</label>
-            <Field type="text" name="textField2" className=' bg-green-200 rounded-xl' />
-            <ErrorMessage name="textField2" />
-          </div>
+      <div className='bg-amber-300 flex flex-col justify-center items-center'>
+        <p>สายการเรียน</p>
+        <p>สายการเรียน จำนวนนักเรียน</p>  
+        <p>IG</p>
+        <p>FB</p>
+        <p>อื่น ๆ</p>
+      </div>      
+      
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={onSubmit}
+            >
+              {({ isSubmitting }) => (
+              <Form className=' flex flex-col justify-center items-center space-y-4 p-5 bg-blue-100'>
+                <div className='flex flex-col'>
+                  <div className='flex'>
+                  <label htmlFor="textField1">การสมัครและการสอบเข้า:</label>
+                  <div className=' size-44 bg-blue-800'></div>
+                  </div>  
+                  <Field type="text" name="textField1" className=' bg-green-200 rounded-xl h-52' />
+                  <ErrorMessage name="textField1" />
+                </div>
 
-          <div>
-            <label htmlFor="textField3">ความน่าสนใจ:</label>
-            <Field type="text" name="textField3" className=' bg-green-200 rounded-xl' />
-            <ErrorMessage name="textField3" />
-          </div>
+                <div className='flex flex-col'>
+                  <div className='flex'>
+                  <label htmlFor="textField2">วิชาหรือหลักสูตรที่เรียนเพิ่มเติม:</label>
+                  <div className=' size-44 bg-blue-800'></div>
+                  </div>  
+                  <Field type="text" name="textField2" className=' bg-green-200 rounded-xl h-52' />
+                  <ErrorMessage name="textField2" />
+                </div>
 
-          <button type="submit" disabled={isSubmitting}>
-          Submit
-          </button>
-        </Form>
-        )}
-      </Formik>
-    </section>
+                <div className='flex flex-col'>
+                  <div className='flex'>
+                  <label htmlFor="textField3">ความน่าสนใจ:</label>
+                  <div className=' size-44 bg-blue-800'></div>
+                  </div>  
+                  <Field type="text" name="textField3" className=' bg-green-200 rounded-xl h-52' />
+                  <ErrorMessage name="textField3" />
+                </div>
+
+                <button type="submit" disabled={isSubmitting}>
+                Submit
+                </button>
+              </Form>
+              )}
+            </Formik>
+            
+      <section className='flex flex-col items-center'>
+        <p>รีวิวจากรุ่นพี่</p>
+        <div className='flex justify-around'>
+            <div>
+              <div className='size-44 bg-blue-800'></div>
+              <p>ชื่อ</p>
+              <p>รุ่น</p>
+              <p>ช่องทางการติดตาม</p>
+            </div>
+            <div className='w-1/3'>
+            มาบอกสูตรเด็ดของอาหารคลีนกันอีกแล้วจ้าอาหารคลีนไม่มีวันเอาท์แน่นอนสำหรับสาวๆที่รักสุขภาพต้องการห่างไกลจากเมนูที่ทำร้ายร่างกายแต่อะไรที่ไม่ดีต่อสุขภาพมักจะอร่อยอยู่เสมอๆอย่างเมนูผัดกะเพราที่ต้องเจอทุกมื้อกลางวันแต่สาวกอาหารคลีนไม่ต้องห่วงว่าจะอดกินกะเพราวันนี้มาแนะนำเมนูกะเพราไก่ไข่ดาวแบบฉบับอาหารคลีนเตรียมตัวเข้าครัวกันได้เลยมาบอกสูตรเด็ดของอาหารคลีนกันอีกแล้วจ้าอาหารคลีนไม่มีวันเอาท์แน่นอนสำหรับสาวๆที่รักสุขภาพต้องการห่างไกลจากเมนูที่ทำร้ายร่างกายแต่อะไรที่ไม่ดีต่อสุขภาพมักจะอร่อยอยู่เสมอๆอย่างเมนูผัดกะเพราที่ต้องเจอทุกมื้อกลางวันแต่สาวกอาหารคลีนไม่ต้องห่วงว่าจะอดกินกะเพราวันนี้มาแนะนำเมนูกะเพราไก่ไข่ดาวแบบฉบับอาหารคลีนเตรียมตัวเข้าครัวกันได้เลย
+            </div>
+        </div>
+        <button>ไอคอนลบ</button>
+      </section>
+
+      <section className='flex flex-col items-center'>
+        <p>รีวิวจากรุ่นพี่</p>
+        <div className='flex justify-around'>
+            <div className='w-1/3'>
+            มาบอกสูตรเด็ดของอาหารคลีนกันอีกแล้วจ้าอาหารคลีนไม่มีวันเอาท์แน่นอนสำหรับสาวๆที่รักสุขภาพต้องการห่างไกลจากเมนูที่ทำร้ายร่างกายแต่อะไรที่ไม่ดีต่อสุขภาพมักจะอร่อยอยู่เสมอๆอย่างเมนูผัดกะเพราที่ต้องเจอทุกมื้อกลางวันแต่สาวกอาหารคลีนไม่ต้องห่วงว่าจะอดกินกะเพราวันนี้มาแนะนำเมนูกะเพราไก่ไข่ดาวแบบฉบับอาหารคลีนเตรียมตัวเข้าครัวกันได้เลยมาบอกสูตรเด็ดของอาหารคลีนกันอีกแล้วจ้าอาหารคลีนไม่มีวันเอาท์แน่นอนสำหรับสาวๆที่รักสุขภาพต้องการห่างไกลจากเมนูที่ทำร้ายร่างกายแต่อะไรที่ไม่ดีต่อสุขภาพมักจะอร่อยอยู่เสมอๆอย่างเมนูผัดกะเพราที่ต้องเจอทุกมื้อกลางวันแต่สาวกอาหารคลีนไม่ต้องห่วงว่าจะอดกินกะเพราวันนี้มาแนะนำเมนูกะเพราไก่ไข่ดาวแบบฉบับอาหารคลีนเตรียมตัวเข้าครัวกันได้เลย
+            </div>
+            <div>
+              <div className='size-44 bg-blue-800'></div>
+              <p>ชื่อ</p>
+              <p>รุ่น</p>
+              <p>ช่องทางการติดตาม</p>
+            </div>
+        </div>
+        <button>ไอคอนลบ</button>
+      </section>
+
+
+    </main>
   );
 };
 
