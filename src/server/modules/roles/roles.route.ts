700f2eefbc3bfea7634ba74007a1705147a20c45
Adisuarn <adisuarn.w@gmail.com>
2024-10-19 00:54:42 +0700

    Change: If-Else to Switch Case

--- src/server/modules/roles/roles.route.ts
+++ src/server/modules/roles/roles.route.ts
@@ -7,41 +7,46 @@ import { createGifted } from '@modules/gifted/gifted.controller'
 import { AllData } from '@libs/data'
 import type { Organization, Club, Program, Gifted } from '@utils/type'
 import { Tag } from '@utils/type'
+import { prisma } from '@utils/db'
 
 export const rolesRouter = new Elysia({ prefix: '/roles' })
   .post('/record', async ({ body, set }) => {
-    if(!body.email.includes('student.triamudom.ac.th')) return error(400, 'Provided Email Not Triam Udom')
-    if(body.tag === 'organization') {
-      if(AllData.Organizations[body.key] === undefined) return error(400, 'Invalid Organization Key')
-      const response = await createOrganization(body as Organization)
-      if(response.success) {
-        set.status = 201
-        return response
+    if(!body.email.includes('student.triamudom.ac.th')) return error(400, 'Provied Email Not Triam Udom')
+    if(!await prisma.user.findUnique({ where: { email: body.email } })) return error(404, 'User Not Found')
+    switch(body.tag) {
+      case Tag.ORGANIZATION: {
+        if(AllData.Organizations[body.key] === undefined) return error(400, 'Invalid Organization Key')
+        const response = await createOrganization(body as Organization)
+        if(response.success) {
+          set.status = 201
+          return response
+        }
       }
-    }
-    if(body.tag === 'club') {
-      if(AllData.Clubs[body.key] === undefined) return error(400, 'Invalid Club Key')
-      const response = await createClub(body as Club)
-      if(response.success) {
-        set.status = 201
-        return response
+      case Tag.CLUB: {
+        if(AllData.Clubs[body.key] === undefined) return error(400, 'Invalid Club Key')
+        const response = await createClub(body as Club)
+        if(response.success) {
+          set.status = 201
+          return response
+        }
       }
-    }
-    if(body.tag === 'program') {
-      if(AllData.Programs[body.key] === undefined) return error(400, 'Invalid Program Key')
-      const response = await createProgram(body as Program)
-      if(response.success) {
-        set.status = 201
-        return response
+      case Tag.PROGRAM: {
+        if(AllData.Programs[body.key] === undefined) return error(400, 'Invalid Program Key')
+        const response = await createProgram(body as Program)
+        if(response.success) {
+          set.status = 201
+          return response
+        }
       }
-    }
-    if(body.tag === 'gifted') {
-      if(AllData.Gifted[body.key] === undefined) return error(400, 'Invalid Gifted Key')
-      const response = await createGifted(body as Gifted)
-      if(response.success) {
-        set.status = 201
-        return response
+      case Tag.GIFTED: {
+        if(AllData.Gifted[body.key] === undefined) return error(400, 'Invalid Gifted Key')
+        const response = await createGifted(body as Gifted)
+        if(response.success) {
+          set.status = 201
+          return response
+        }
       }
+      default: return error(400, 'Invalid Tag')
     }
   },
   {
