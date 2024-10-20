bf311d23030f41e77b93c62bdbb2735a2fcf7235
Sirut Krizruenont <siruj2201@gmail.com>
2024-10-20 22:30:32 +0700

    account page frontend v2

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -3,7 +3,7 @@ import LogoutButton from "@/components/LogoutButton";
 import { getUser } from "@/server/middlewares/derive";
 import { redirect } from "next/navigation";
 import { MdOutlineAccountCircle } from "react-icons/md";
-import { FaWpforms } from "react-icons/fa6";
+import { FaLessThanEqual, FaWpforms } from "react-icons/fa6";
 import Link from "next/link";
 import apiFunction from "@/components/api";
 import Section from "@/vectors/dashboard/Section";
@@ -14,22 +14,32 @@ const AccountPage = async () => {
   const data = response.data;
 
   const test = {
-    name: "Ronaldo",
+    name: "ชมรมไพ่เพื่อการเรียนรู้",
     key: "gifted",
     members: 30,
   };
 
   const submittedInit = true;
-  const submittedForm = true;
+  const submittedForm = false;
+  const checked = true;
+  const passed = true;
 
   console.log(data);
   return (
     <section className="flex h-screen flex-col items-center justify-center space-y-4 text-formText">
-      {submittedInit ? <p>ข้อมูลหน่วยงาน</p> : <p className="text-xl">ยินดีตอนรับ</p>}
+      {submittedInit ? (
+        <p>ข้อมูลหน่วยงาน</p>
+      ) : (
+        <p className="text-xl">ยินดีตอนรับ</p>
+      )}
       <p className="relative -space-y-2 bg-gradient-to-b from-heroFirst via-heroMiddle to-greenText bg-clip-text text-xl font-bold text-transparent sm:flex sm:text-2xl md:text-4xl">
         {submittedInit ? <>{test.name}</> : <>{data.name}</>}
       </p>
-      {submittedInit ? <p className="opacity-70">จำนวนสมาชิก {test.members} คน</p> : ''}
+      {submittedInit ? (
+        <p className="opacity-70">จำนวนสมาชิก {test.members} คน</p>
+      ) : (
+        ""
+      )}
       <Section />
       {submittedInit ? (
         <Link href={`/editingform/${test.key}`}>
@@ -47,7 +57,33 @@ const AccountPage = async () => {
         </Link>
       )}
 
-      {submittedForm ? <p>submitted :</p> : <p>สถานะ :</p>}
+      {submittedInit ? (
+        submittedForm ? (
+          checked ? (
+            passed ? (
+              <div className="flex items-center justify-center space-x-1">
+                <div className="h-4 w-4 rounded-full bg-[#19C57C]"></div>
+                <p className="text-[#19C57C]">ผ่านการตรวจสอบ</p>
+              </div>
+            ) : (
+              <div className="flex items-center justify-center space-x-1">
+                <div className="h-4 w-4 rounded-full bg-[#E80808]"></div>
+                <p className="text-[#E80808]">ไม่ผ่านการตรวจสอบ</p>
+              </div>
+            )
+          ) : (
+            <div className="flex items-center justify-center space-x-1">
+              <div className="h-4 w-4 rounded-full bg-[#FCB52B]"></div>
+              <p className="text-[#FCB52B]">อยู่ระหว่างการตรวจสอบ</p>
+            </div>
+          )
+        ) : (
+          <p>ยังไม่ได้ส่งแบบฟอร์ม</p>
+        )
+      ) : (
+        ""
+      )}
+
       <div>
         <p className="text-2xl">แก้ไข้ข้อมูลหน่วยงาน</p>
         <p className="opacity-70">ข้อมูลจะแสดงผลในหน้าเว็บไซต์</p>
