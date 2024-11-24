3307b6c7f5438302b980aaa81630ed820cc166f1
Adisuarn <adisuarn.w@gmail.com>
2024-11-24 20:29:50 +0700

    update route setting

--- src/app/api/[[...routes]]/route.ts
+++ src/app/api/[[...routes]]/route.ts
@@ -1,6 +1,8 @@
 import { elysiaApp } from '@/server/elysia'
 
 export const maxDuration = 60;
+export const dynamic = 'force-dynamic';
+export const revalidate = '0';
 
 export const GET = elysiaApp.handle;
 export const POST = elysiaApp.handle;
