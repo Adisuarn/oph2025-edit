4c63cac874b9385e0f68a5ede0f07d01d0e14fda
Adisuarn <adisuarn.w@gmail.com>
2024-11-24 19:33:54 +0700

    setting routes

--- src/app/api/[[...routes]]/route.ts
+++ src/app/api/[[...routes]]/route.ts
@@ -1,7 +1,10 @@
 import { elysiaApp } from '@/server/elysia'
 
-export const maxDuration = 60
-export const GET = elysiaApp.handle
-export const POST = elysiaApp.handle
-export const PATCH = elysiaApp.handle
-export const DELETE = elysiaApp.handle
+export const maxDuration = 60;
+export const dynamic = 'force-dynamic'
+export const revalidate = 0;
+
+export const GET = elysiaApp.handle;
+export const POST = elysiaApp.handle;
+export const PATCH = elysiaApp.handle;
+export const DELETE = elysiaApp.handle;
