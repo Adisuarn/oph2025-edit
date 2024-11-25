45e92fc8d2c6c3d907295a2aa333f4f3ddc64063
Adisuarn <adisuarn.w@gmail.com>
2024-11-25 21:05:55 +0700

    reverting settings

--- src/app/api/[[...routes]]/route.ts
+++ src/app/api/[[...routes]]/route.ts
@@ -1,8 +1,6 @@
 import { elysiaApp } from '@/server/elysia'
 
 export const maxDuration = 60;
-export const dynamic = 'force-dynamic';
-export const revalidate = '0';
 
 export const GET = elysiaApp.handle;
 export const POST = elysiaApp.handle;
